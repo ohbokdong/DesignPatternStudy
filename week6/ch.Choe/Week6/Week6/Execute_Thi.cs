8c7ea067d01dd178c47e342bc3a156a2ef19b51d
ccg1120 <ccg.1120@daum.net>
2020-02-09 00:16:13 +0900

    쓰레드풀 구현. 커멘드를 이용한 상태 저장

---
+++ week6/ch.Choe/Week6/Week6/Execute_Thi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Week6
+{
+    [Serializable]
+    class Execute_Thi
+    {
+        public int num = 0;
+        public string Class_Name = "Execute_Thi";
+
+        public void PrintName()
+        {
+            Console.WriteLine("Class = " + Class_Name + ", " + num + " Start");
+            Thread.Sleep(1500);
+            Console.WriteLine("Class = " + Class_Name + " End");
+            num++;
+        }
+    }
+}
