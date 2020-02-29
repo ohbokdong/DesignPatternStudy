cfc8fbd7b8aa397fd3c7d18aa77110bbe0d5b81d
ccg1120 <ccg.1120@daum.net>
2020-03-01 03:42:39 +0900

    템플릿

---
+++ week8/ch.Choe/Week8/Coffe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week8
+{
+    class Coffe : CaffeBeverage
+
+    {
+        protected override void Add()
+        {
+            Console.WriteLine("Coffe Add");    
+        }
+
+
+
+        protected override void Brew()
+        {
+            Console.WriteLine("Coffe Brew");
+        }
+
+
+    }
+}
---
+++ week8/ch.Choe/Week8/Tea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week8
+{
+    class Tea : CaffeBeverage
+    {
+        protected override void Add()
+        {
+            Console.WriteLine("Tea Add");
+        }
+
+        protected override void Brew()
+        {
+            Console.WriteLine("Tea Brew");
+        }
+    }
+}
