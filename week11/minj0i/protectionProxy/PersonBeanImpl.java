3627801165edb37948befbda3858f40c596f007e
minj0i <h_hoh@naver.com>
2020-03-28 15:45:05 +0900

    proxy끝

---
+++ week11/minj0i/protectionProxy/PersonBeanImpl.java
@@ -0,0 +1,52 @@
+package week11.minj0i.protectionProxy;
+
+public class PersonBeanImpl implements PersonBean{
+	String name;
+	String gender;
+	String interests;
+	int rating;
+	int ratingCount = 0;
+	
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String getGender() {
+		return gender;
+	}
+
+	@Override
+	public String getInterests() {
+		return interests;
+	}
+
+	@Override
+	public int getHotOrNotRating() {
+		if (ratingCount == 0) return 0;
+		return (rating/ratingCount);
+	}
+
+	@Override
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	@Override
+	public void setInterests(String interests) {
+		this.interests = interests;
+	}
+
+	@Override
+	public void setHotOrNotRating(int rating) {
+		this.rating += rating;
+		ratingCount++;
+	}
+	
+}
