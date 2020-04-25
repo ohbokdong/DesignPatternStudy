aec1368f612a8deb9fb5c5ed95565d6fd5c2ee9c
rlawjddbs <zeongyun@gmail.com>
2020-04-25 14:05:56 +0900

    코드수정

--- week11/rlawjddbs/proxy/GumballMonitor.java
+++ week11/rlawjddbs/proxy/GumballMonitor.java
@@ -12,7 +12,7 @@ public class GumballMonitor {
 	public void report() {
 		try {
 			System.out.println("뽑기 기계 위치 : " + machine.getLocation());
-			System.out.println("현재 재고 : " + machine.getCount());
+			System.out.println("현재 재고 : " + machine.getCount() + "개");
 			System.out.println("현재 상태 : " + machine.getState());
 		} catch (RemoteException e) {
 			e.getMessage();
